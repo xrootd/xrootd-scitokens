fb39aa2b2c2170b54f1e66192e4a20d00ed3dc73
Brian Bockelman <bbockelm@cse.unl.edu>
2019-04-27 12:43:56 -0500

    Initial changes to remove python code.
    Sketched out a few of the necessary C++ pieces, but these aren't
    implemented yet.

--- src/scitokens.cpp
+++ src/scitokens.cpp
@@ -5,15 +5,11 @@
 #include "XrdSys/XrdSysLogger.hh"
 #include "XrdVersion.hh"
 
-#include <boost/python.hpp>
-
 #include <map>
 #include <memory>
 #include <mutex>
 #include <string>
 #include <vector>
-
-#include <dlfcn.h>
 
 XrdVERSIONINFO(XrdAccAuthorizeObject, XrdAccSciTokens);
 
@@ -26,22 +22,11 @@ extern XrdAccAuthorize *XrdAccDefaultAuthorizeObject(XrdSysLogger   *lp,
                                                      XrdVersionInfo &myVer);
 
 
-static std::string
-handle_pyerror()
-{
-    PyObject *exc,*val,*tb;
-    boost::python::object formatted_list, formatted;
-    PyErr_Fetch(&exc,&val,&tb);
-    boost::python::handle<> hexc(exc), hval(boost::python::allow_null(val)), htb(boost::python::allow_null(tb));
-    boost::python::object traceback(boost::python::import("traceback"));
-    boost::python::object format_exception(traceback.attr("format_exception"));
-    formatted_list = format_exception(hexc,hval,htb);
-    formatted = boost::python::str("\n").join(formatted_list);
-    return boost::python::extract<std::string>(formatted);
-}
+namespace {
 
+typedef std::vector<std::pair<Access_Operation, std::string>> AccessRulesRaw;
 
-static inline uint64_t monotonic_time() {
+inline uint64_t monotonic_time() {
   struct timespec tp;
 #ifdef CLOCK_MONOTONIC_COARSE
   clock_gettime(CLOCK_MONOTONIC_COARSE, &tp);
@@ -51,8 +36,7 @@ static inline uint64_t monotonic_time() {
   return tp.tv_sec + (tp.tv_nsec >= 500000000);
 }
 
-
-static XrdAccPrivs AddPriv(Access_Operation op, XrdAccPrivs privs)
+XrdAccPrivs AddPriv(Access_Operation op, XrdAccPrivs privs)
 {
     int new_privs = privs;
     switch (op) {
@@ -98,6 +82,9 @@ static XrdAccPrivs AddPriv(Access_Operation op, XrdAccPrivs privs)
     return static_cast<XrdAccPrivs>(new_privs);
 }
 
+}
+
+
 class XrdAccRules
 {
 public:
@@ -120,42 +107,33 @@ public:
 
     bool expired() const {return monotonic_time() > m_expiry_time;}
 
-    void parse(boost::python::list results) {
-        int cache_len = boost::python::len(results);
-        for (int idx=0; idx<cache_len; idx++) {
-            boost::python::object entry = results[idx];
-            Access_Operation aop = boost::python::extract<Access_Operation>(entry[0]);
-            std::string path = boost::python::extract<std::string>(entry[1]);
-            m_rules.emplace_back(aop, path);
+    void parse(const AccessRulesRaw &rules) {
+        m_rules.reserve(rules.size());
+        for (const auto &entry : rules) {
+            m_rules.emplace_back(entry.first, entry.second);
         }
     }
 
     const std::string & get_username() const {return m_username;}
 
 private:
-    std::vector<std::pair<Access_Operation, std::string>> m_rules;
+    AccessRulesRaw m_rules;
     uint64_t m_expiry_time{0};
     const std::string m_username;
 };
+
 
 class XrdAccSciTokens : public XrdAccAuthorize
 {
 public:
     XrdAccSciTokens(XrdSysLogger *lp, const char *parms, std::unique_ptr<XrdAccAuthorize> chain) :
-        m_module(boost::python::import("scitokens_xrootd")),
         m_chain(std::move(chain)),
+        m_parms(parms ? parms : ""),
         m_next_clean(monotonic_time() + m_expiry_secs),
         m_log(lp, "scitokens_")
     {
         m_log.Say("++++++ XrdAccSciTokens: Initialized SciTokens-based authorization.");
-        if (parms) {
-            m_log.Say("Initializing python module with params ", parms);
-            m_module.attr("init")(parms);
-        } else {
-            m_log.Say("Initializing python module with no configuration parameters");
-            m_module.attr("init")();
-        }
-        m_log.Say("Finished python module initialization.");
+        Reconfig();
     }
 
     virtual ~XrdAccSciTokens() {}
@@ -163,7 +141,7 @@ public:
     virtual XrdAccPrivs Access(const XrdSecEntity *Entity,
                                   const char         *path,
                                   const Access_Operation oper,
-                                        XrdOucEnv       *env)
+                                        XrdOucEnv       *env) override
     {
         const char *authz = env ? env->Get("authz") : nullptr;
         if (authz == nullptr) {
@@ -180,19 +158,21 @@ public:
             }
         }
         if (!access_rules) {
-            std::lock_guard<std::mutex> guard(m_mutex);
             try {
-                boost::python::object retval = m_module.attr("generate_acls")(authz);
-                boost::python::list cache = boost::python::list(retval[1]);
-                std::string username = boost::python::extract<std::string>(retval[2]);
-                //m_log.Emsg("Access", "SciTokens library returned suggested username", username.c_str());
-                uint64_t cache_expiry = boost::python::extract<uint64_t>(retval[0]);
-                access_rules.reset(new XrdAccRules(now + cache_expiry, username));
-                access_rules->parse(cache);
-            } catch (boost::python::error_already_set) {
-                m_log.Emsg("Access", "Error generating ACLs for authorization", handle_pyerror().c_str());
+		uint64_t cache_expiry;
+		AccessRulesRaw rules;
+                std::string username;
+                if (GenerateAcls(authz, cache_expiry, rules, username)) {
+                    access_rules.reset(new XrdAccRules(now + cache_expiry, username));
+                    access_rules->parse(rules);
+                } else {
+                    return m_chain ? m_chain->Access(Entity, path, oper, env) : XrdAccPriv_None;
+                }
+            } catch (std::exception &exc) {
+                m_log.Emsg("Access", "Error generating ACLs for authorization", exc.what());
                 return m_chain ? m_chain->Access(Entity, path, oper, env) : XrdAccPriv_None;
             }
+            std::lock_guard<std::mutex> guard(m_mutex);
             m_map[authz] = access_rules;
         }
         const std::string &username = access_rules->get_username();
@@ -207,18 +187,26 @@ public:
                       const XrdSecEntity    *Entity,
                       const char            *path,
                       const Access_Operation oper,
-                            XrdOucEnv       *Env=0)
+                            XrdOucEnv       *Env=0) override
     {
         return 0;
     }
 
     virtual int         Test(const XrdAccPrivs priv,
-                             const Access_Operation oper)
+                             const Access_Operation oper) override
     {
         return 0;
     }
 
 private:
+
+    bool GenerateAcls(const std::string &authz, uint64_t &cache_expiry, AccessRulesRaw &rules, std::string &username) {
+        return false;
+    }
+
+    void Reconfig()
+    {
+    }
 
     void Check(uint64_t now)
     {
@@ -233,8 +221,8 @@ private:
 
     std::mutex m_mutex;
     std::map<std::string, std::shared_ptr<XrdAccRules>> m_map;
-    boost::python::object m_module;
     std::unique_ptr<XrdAccAuthorize> m_chain;
+    std::string m_parms;
     uint64_t m_next_clean{0};
     XrdSysError m_log;
 
@@ -247,36 +235,8 @@ XrdAccAuthorize *XrdAccAuthorizeObject(XrdSysLogger *lp,
                                        const char   *cfn,
                                        const char   *parm)
 {
-    // First, try to initialize the embedded python.
-    if (!Py_IsInitialized())
-    {
-        char pname[] = "xrootd";
-        Py_SetProgramName(pname);
-        Py_InitializeEx(0);
-    }
-    // We need to reload the current shared library:
-    //   - RTLD_GLOBAL instructs the loader to put everything into the global symbol table.  Python
-    //     requires this for several modules.
-    //   - RTLD_NOLOAD instructs the loader to actually reload instead of doing an initial load.
-    //   - RTLD_NODELETE instructs the loader to not unload this library -- we need python kept in
-    //     memory!
-    void *handle = dlopen("libXrdAccSciTokens-4.so", RTLD_GLOBAL|RTLD_NODELETE|RTLD_NOLOAD|RTLD_LAZY);
-    if (handle == nullptr) {
-        XrdSysError eDest(lp, "scitokens_");
-        eDest.Emsg("XrdAccSciTokens", "Failed to reload python libraries:", dlerror());
-        return nullptr;
-    }
-    dlclose(handle);  // Per use of RTLD_NODELETE|RTLD_NOLOAD, does not actually unload this library!
-
     std::unique_ptr<XrdAccAuthorize> def_authz(XrdAccDefaultAuthorizeObject(lp, cfn, parm, compiledVer));
-    XrdAccSciTokens *authz{nullptr};
-    try {
-        authz = new XrdAccSciTokens(lp, parm, std::move(def_authz));
-    } catch (boost::python::error_already_set) {
-        XrdSysError eDest(lp, "scitokens_");
-        eDest.Emsg("XrdAccSciTokens", "Python failure initializing module:", handle_pyerror().c_str());
-    }
-    return authz;
+    return new XrdAccSciTokens(lp, parm, std::move(def_authz));
 }
 
 }
